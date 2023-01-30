ea0b5cd8bd763c836288fa0305f5669595206ff6
ZizmanTK <abdoulazizmaazoumanomi@gmail.com>
2023-01-30 23:40:39 +0100

    Test du classifieur apres division des donnes d'entrainement

--- src/Tests.py
+++ src/Tests.py
@@ -1,9 +1,30 @@
 # Tests
 from Data_frame import file_info
-from SVM import vec, classifier, encode_sentence
+from SVM import vec, classifier, encode_sentence, df_test, docs_tests_dates, docs_starts_test,sent_dates
 import numpy as np
 
 test_folder_path = "../test_folder_predilex/test_folder/txt_files/"
+
+
+def get_dates2(sentences, dates, word_vec, clf):
+    """
+    This function takes a document, a classifier and a vector of words and output the predicted dates of accident and consolidation
+    """
+    x = np.zeros([len(sentences), len(vec)])
+    for i in range(len(sentences)):
+        x[i, :] = encode_sentence(sentences[i], word_vec)
+    results = clf.decision_function(x)
+    acc = np.argmax(results[:, 0])
+    cons = np.argmax(results[:, 1])
+    if results[acc, 0] > 0:
+        date_accident = dates[acc]
+    else:
+        date_accident = "n.a."
+    if results[cons, 1] > 0:
+        date_consolidation = dates[cons]
+    else:
+        date_consolidation = "n.c."
+    return date_accident, date_consolidation
 
 
 def get_dates(file_name, word_vec, clf):
@@ -16,26 +37,14 @@ def get_dates(file_name, word_vec, clf):
     sentences,l,dates = file_info(file_name, (00, 00, 0000))
     print("DATES")
     print(dates)
-    x = np.zeros([len(sentences),len(vec)])
-    for i in range(len(sentences)):
-        x[i,:] = encode_sentence(sentences[i],word_vec)
-    results = clf.decision_function(x)
-    print(results.shape)
-    print(len(dates))
-    print(len(sentences))
-    acc = np.argmax(results[:,0])
-    cons = np.argmax(results[:,1])
-    if results[acc,0]>0:
-        date_accident = dates[acc]
-    else:
-        date_accident = "n.a."
-    if results[cons,1]>0:
-        date_consolidation = dates[cons]
-    else:
-        date_consolidation = "n.c."
-    print(sentences[acc])
-    print(sentences[cons])
-    return date_accident,date_consolidation
+    return get_dates2(sentences,dates,word_vec,clf)
 
 
-print(get_dates("Aix-en-provence_619114.txt",vec,classifier))
+test_sentences = np.array(df_test['Sentences'])
+# print(get_dates("Agen_400518.txt",vec,classifier))
+for i in range(len(docs_tests_dates)-1):
+    if (len(test_sentences[docs_starts_test[i]:docs_starts_test[i+1]]))==0:continue
+    prediction = get_dates2(test_sentences[docs_starts_test[i]:docs_starts_test[i+1]],sent_dates[docs_starts_test[i]:docs_starts_test[i+1]],vec,classifier)
+    print("Reelles  :", docs_tests_dates[i+1])
+    print("Predites :", prediction)
+    print("====================================================")
