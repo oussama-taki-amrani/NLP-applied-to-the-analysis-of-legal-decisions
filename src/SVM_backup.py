988d911ac02c3d23b8ccac1afd23d822f0bf202e
ZizmanTK <abdoulazizmaazoumanomi@gmail.com>
2023-01-31 09:31:04 +0100

    Message

--- src/SVM_backup.py
+++ src/SVM_backup.py
@@ -1,13 +1,12 @@
-from Data_frame import df,docs_starts,text_dates, all_dates
+from Data_frame import df
 import numpy as np
 from sklearn import svm
-import json
 
 
 def compute_words_occurence(labelized_sentences):
 
     occurences = [{},{},{}]
-    for i in range(len(labelized_sentences['Label'])):
+    for i in range(len(labelized_sentences)):
         sentence = labelized_sentences['Sentences'][i]
         label = labelized_sentences['Label'][i]
         for word in sentence:
@@ -36,6 +35,18 @@ def get_best_scores(scores,nb=50):
     return vec,sorted_scores
 
 
+occurences = compute_words_occurence(df)
+vec,sorted_scores = get_best_scores(scores_from_occurences(occurences))
+
+for k,v in sorted_scores[0].items():
+    print(k,(30-len(k))*" ",":",v,end="      ")
+    print(occurences[0][k],"       ",occurences[1][k]+occurences[2][k])
+for k,v in sorted_scores[1].items():
+    print(k,(30-len(k))*" ",":",v,end="      ")
+    print(occurences[1][k],"       ",occurences[0][k]+occurences[2][k])
+for k,v in sorted_scores[2].items():
+    print(k,(30-len(k))*" ",":",v,end="      ")
+    print(occurences[2][k],"       ",occurences[1][k]+occurences[0][k])
 
 #Bag of words
 def encode_sentence(sentence,words_vec):
@@ -45,49 +56,16 @@ def encode_sentence(sentence,words_vec):
     return x
 
 
-
-#Dividing the data frame into a training and a validation set
-nb_docs = len(docs_starts)-1 #The total number of documents in the train folder
-train_proportion = .7 # The proportion of documents we want for the training
-nb_docs_train = int(train_proportion*nb_docs)
-nb_docs_test = nb_docs - nb_docs_train
-all_sentences = np.array(df['Sentences'])
-all_labels = np.array(df['Label'])
-df_train = {'Sentences':all_sentences[:docs_starts[nb_docs_train]],'Label':all_labels[:docs_starts[nb_docs_train]]}
-df_test = {'Sentences':all_sentences[docs_starts[nb_docs_train]:],'Label':all_labels[docs_starts[nb_docs_train]:]}
-docs_starts_test = np.array(docs_starts)[nb_docs_train:] - docs_starts[nb_docs_train]
-print("DOCS:", docs_starts_test[:10])
-print("S:", len(text_dates)+1,"P: ",nb_docs)
-docs_tests_dates = [text_dates[i] for i in range(nb_docs_train-1,nb_docs-1)]
-sent_dates = []
-for i in range(docs_starts[nb_docs_train],len(all_sentences)):
-    sent_dates.append(all_dates[i])
-
-
-
-#Computing scores
-occurences = compute_words_occurence(df_train)
-vec, sorted_scores = get_best_scores(scores_from_occurences(occurences))
-with open("../outputs/sorted_scores_accident.txt", 'w') as file:
-    file.write(json.dumps(sorted_scores[0]))
-with open("../outputs/sorted_scores_consolidation.txt", 'w') as file:
-    file.write(json.dumps(sorted_scores[1]))
-with open("../outputs/sorted_scores_other.txt", 'w') as file:
-    file.write(json.dumps(sorted_scores[2]))
-
-
-
 # Setting up the training datas
-x_train = np.zeros([len(df_train['Sentences']), len(vec)])
-y_train = np.zeros(len(df_train['Label']))
-print(df_train['Label'])
-for i in range(len(df_train['Label'])):
-    x_train[i,:] = encode_sentence(df_train['Sentences'][i],vec)
-    y_train[i] = df_train['Label'][i]
+x_train = np.zeros([len(df),len(vec)])
+y_train = np.zeros(len(df))
+for i in range(len(df)):
+    x_train[i,:] = encode_sentence(df['Sentences'][i],vec)
+    y_train[i] = df['Label'][i]
+
 #Training the SVM
 classifier = svm.SVC(kernel='linear')
 print("Start learning")
-print("taille",len(y_train))
 classifier.fit(x_train,y_train)
 
 
